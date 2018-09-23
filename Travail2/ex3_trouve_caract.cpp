a12846dfe6c1a26eb93627d2a95a0d1cd4b62df8
Marc St-Pierre <marcst-pierre@motivateco.com>
2018-09-22 21:40:49 -0400

    implementation complete de ex4

--- Travail2/ex3_trouve_caract.cpp
+++ Travail2/ex3_trouve_caract.cpp
@@ -10,72 +10,84 @@ char caract;
 int compte_caracteres(char mot[nbr_caractere_maximal]){
 	int i = 0;
 	while(mot[i] != '\0'){
+	//	cout << i << ": " << mot[i] << endl;
 		i++;
 	}
-	return i;
+	return i++;
 }
 
 bool valide_mot_saisie(char mot[nbr_caractere_maximal]){
     if (compte_caracteres(mot) < nbr_caractere_minimal){
             cout << "Le mot de contenir entre 10 et 100 caracteres." << endl;
         return false;
-    }
+    } else {
+	return true;
+	}
 }
 
 char* demande_mot_et_caractere(){
-	char mot[nbr_caractere_maximal];
+    char motsaisie[nbr_caractere_maximal];
     cout << "Veuillez entrer un mot de 10 caracteres ou plus : ";
-    cin >> mot;
-    while(!valide_mot_saisie(mot)){
+    cin >> motsaisie;
+    while(!valide_mot_saisie(motsaisie)){
         cout << "Veuillez entrer un mot de 10 caracteres ou plus : ";
-        cin >> mot;
+        cin >> motsaisie;
     }
     cout << "Veuillez entrer un caractere a rechercher :";
     cin >> caract;
-	return mot;
+	return motsaisie;
 }
 
-int* popule_indexes(char mot[nbr_caractere_maximal]){
+int* popule_indexes(char *mot){
    int indexes[nbr_caractere_maximal];
-	int i = 0;
+   int *p = indexes;
     int current_index = 0;
-    while (mot[i] != '\0'){
-        if (toupper(mot[i]) == toupper(caract)){
-            indexes[current_index] = i;
+	int indexe_mot = 0;
+    while (*mot != '\0'){
+        if (toupper(*mot) == toupper(caract)){
+//		cout << "on ajoute " << indexe_mot << " a la table indexes a la position " << current_index << endl;
+		p = indexes + current_index; *p = indexe_mot;
             current_index++;
         }
-        i++;
+	    mot++;
+	indexe_mot++;
     }
-	return indexes;
-}
-
-void affiche_indexes(int indexes[nbr_caractere_maximal]){
-	cout << "Voici la liste des indices ou se trouv le caractere recherche : " << endl;
-	int i = 0;
-	while(indexes[i] != '\0'){
-		cout << indexes[i] << " " ;
-		i++;
+	cout << "les indexes ou es touvent des " << caract << " dans le mot :" << endl;
+	for (int j = 0; j<current_index; j++){
+		cout << indexes[j] << " ";
 	}
+	cout << endl;
+	return p;
 }
 
-void affiche_mot_formate(char mot[nbr_caractere_maximal]){
-	int i=0;
-	while(mot[i] != '\0'){
-		if (toupper(mot[i]) == toupper(caract)){
-			cout << toupper(mot[i]);
+void affiche_indexes(int * indexes){
+	cout << "Voici la liste des indices ou se trouve le caractere recherche : " << endl;
+	while(*indexes != '\0'){
+		cout << *indexes << " " ;
+		indexes++;
+	}
+	cout << endl;
+}
+
+void affiche_mot_formate(char *mot){
+	while(*mot != '\0'){
+		if (toupper(*mot) == toupper(caract)){
+			cout << char(toupper(*mot));
 		}
 		else {
-			cout << mot[i];
+			cout << char(*mot);
 		}
-		i++;
+	mot++;
 	}
+	cout << endl;
 }
 
 int main(){
 	char mot[nbr_caractere_maximal];
+	//mot = demande_mot_et_caractere(mot);
 	strcpy(mot,  demande_mot_et_caractere());
-	int indexes[nbr_caractere_maximal] = populate_indexes(mot);
-	affiche_indexes(indexes);
+	 int *indexes =  popule_indexes(mot);
+//	affiche_indexes(indexes);
 	affiche_mot_formate(mot);
 	return 0;
 }
