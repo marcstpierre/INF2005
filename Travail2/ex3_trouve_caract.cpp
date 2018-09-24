6e67b3863ad6ab1889456d3bedda4c5869b9d5bb
Marc St-Pierre <marcst-pierre@motivateco.com>
2018-09-23 20:28:23 -0400

    ajout de commmentaires et cleanup

--- Travail2/ex3_trouve_caract.cpp
+++ Travail2/ex3_trouve_caract.cpp
@@ -7,15 +7,16 @@ int nbr_caractere_maximal = 100;
 
 char caract;
 
+//méthode qui compte le nombre d'un tableau de caractères 
 int compte_caracteres(char mot[nbr_caractere_maximal]){
 	int i = 0;
 	while(mot[i] != '\0'){
-	//	cout << i << ": " << mot[i] << endl;
 		i++;
 	}
 	return i++;
 }
 
+//c'est ici qu'on valide que le mot saisie contient le nombre minimal de caractères.
 bool valide_mot_saisie(char mot[nbr_caractere_maximal]){
     if (compte_caracteres(mot) < nbr_caractere_minimal){
             cout << "Le mot de contenir entre 10 et 100 caracteres." << endl;
@@ -25,6 +26,7 @@ bool valide_mot_saisie(char mot[nbr_caractere_maximal]){
 	}
 }
 
+//Méthode qui gère la saisie et la validation du mot à traiter.
 char* demande_mot_et_caractere(){
     char motsaisie[nbr_caractere_maximal];
     cout << "Veuillez entrer un mot de 10 caracteres ou plus : ";
@@ -38,20 +40,23 @@ char* demande_mot_et_caractere(){
 	return motsaisie;
 }
 
+
+//Cette méthode boucle dans le mot et trouve l'indexe des lettres qui sont égales à la lettre recherchée (on compare les majuscules).
 int* popule_indexes(char *mot){
    int indexes[nbr_caractere_maximal];
-   int *p = indexes;
+   int *p = indexes; // on est en cpp, utilisons donc un pointeur pour faire référence au tableau d'indexes.
     int current_index = 0;
 	int indexe_mot = 0;
     while (*mot != '\0'){
         if (toupper(*mot) == toupper(caract)){
-//		cout << "on ajoute " << indexe_mot << " a la table indexes a la position " << current_index << endl;
-		p = indexes + current_index; *p = indexe_mot;
+		p = indexes + current_index;//On déplace le pointeur pour pointer vers l'indice dans le tableau d'indexes. 
+		*p = indexe_mot;//C'est là qu'on assigne l'indice trouvé au tableau d'indexes.
             current_index++;
         }
 	    mot++;
 	indexe_mot++;
     }
+	//Affichons les indexes trouvés.
 	cout << "les indexes ou es touvent des " << caract << " dans le mot :" << endl;
 	for (int j = 0; j<current_index; j++){
 		cout << indexes[j] << " ";
@@ -60,15 +65,7 @@ int* popule_indexes(char *mot){
 	return p;
 }
 
-void affiche_indexes(int * indexes){
-	cout << "Voici la liste des indices ou se trouve le caractere recherche : " << endl;
-	while(*indexes != '\0'){
-		cout << *indexes << " " ;
-		indexes++;
-	}
-	cout << endl;
-}
-
+//C'est ici qu'on affiche le mot saisie avec les caractères trouvés en majuscule.
 void affiche_mot_formate(char *mot){
 	while(*mot != '\0'){
 		if (toupper(*mot) == toupper(caract)){
@@ -84,10 +81,8 @@ void affiche_mot_formate(char *mot){
 
 int main(){
 	char mot[nbr_caractere_maximal];
-	//mot = demande_mot_et_caractere(mot);
 	strcpy(mot,  demande_mot_et_caractere());
 	 int *indexes =  popule_indexes(mot);
-//	affiche_indexes(indexes);
 	affiche_mot_formate(mot);
 	return 0;
 }
